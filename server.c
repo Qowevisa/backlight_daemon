c25ab8165eb30fba4071c8df0ef163b8d85d8302
Qowevisa <qowevisa@gmail.com>
2022-07-24 17:07:43 +0300

    It is working

--- server.c
+++ server.c
@@ -59,106 +59,109 @@ int main() {
 
         if (setuid(1000) != 0) {
             perror("setuid() error");
-        } else {
+            return 1;
+        }
+
+        /***************************************/
+        /* Set up the UNIX sockaddr structure  */
+        /* by using AF_UNIX for the family and */
+        /* giving it a filepath to bind to.    */
+        /*                                     */
+        /* Unlink the file so the bind will    */
+        /* succeed, then bind to that file.    */
+        /***************************************/
+        server_sockaddr.sun_family = AF_UNIX;   
+        strcpy(server_sockaddr.sun_path, SOCK_PATH); 
+        len = sizeof(server_sockaddr);
         
-            /***************************************/
-            /* Set up the UNIX sockaddr structure  */
-            /* by using AF_UNIX for the family and */
-            /* giving it a filepath to bind to.    */
-            /*                                     */
-            /* Unlink the file so the bind will    */
-            /* succeed, then bind to that file.    */
-            /***************************************/
-            server_sockaddr.sun_family = AF_UNIX;   
-            strcpy(server_sockaddr.sun_path, SOCK_PATH); 
-            len = sizeof(server_sockaddr);
-            
-            unlink(SOCK_PATH);
-            rc = bind(server_sock, (struct sockaddr *) &server_sockaddr, len);
-            if (rc == -1){
-                printf("BIND ERROR: %s\n", strerror(errno));
+        unlink(SOCK_PATH);
+        rc = bind(server_sock, (struct sockaddr *) &server_sockaddr, len);
+        if (rc == -1){
+            printf("BIND ERROR: %s\n", strerror(errno));
+            close(server_sock);
+            exit(1);
+        }
+        
+        while (1) {
+            memset(buf, 0, 256);
+
+            /*********************************/
+            /* Listen for any client sockets */
+            /*********************************/
+            rc = listen(server_sock, backlog);
+            if (rc == -1){ 
+                printf("LISTEN ERROR: %s\n", strerror(errno));
                 close(server_sock);
                 exit(1);
             }
-            
-            while (1) {
-                memset(buf, 0, 256);
-
-                /*********************************/
-                /* Listen for any client sockets */
-                /*********************************/
-                rc = listen(server_sock, backlog);
-                if (rc == -1){ 
-                    printf("LISTEN ERROR: %s\n", strerror(errno));
-                    close(server_sock);
-                    exit(1);
-                }
-                printf("socket listening...\n");
-            
-                /*********************************/
-                /* Accept an incoming connection */
-                /*********************************/
-                client_sock = accept(server_sock, (struct sockaddr *) &client_sockaddr, &len);
-                if (client_sock == -1){
-                    printf("ACCEPT ERROR: %s\n", strerror(errno));
-                    close(server_sock);
-                    close(client_sock);
-                    exit(1);
-                }
-                
-                /****************************************/
-                /* Get the name of the connected socket */
-                /****************************************/
-                len = sizeof(client_sockaddr);
-                rc = getpeername(client_sock, (struct sockaddr *) &client_sockaddr, &len);
-                if (rc == -1){
-                    printf("GETPEERNAME ERROR: %s\n", strerror(errno));
-                    close(server_sock);
-                    close(client_sock);
-                    exit(1);
-                }
-                else {
-                    printf("Client socket filepath: %s\n", client_sockaddr.sun_path);
-                }
-            
-                /************************************/
-                /* Read and print the data          */
-                /* incoming on the connected socket */
-                /************************************/
-                printf("waiting to read...\n");
-                bytes_rec = recv(client_sock, buf, sizeof(buf), 0);
-                if (bytes_rec == -1){
-                    printf("RECV ERROR: %s\n", strerror(errno));
-                    close(server_sock);
-                    close(client_sock);
-                    exit(1);
-                }
-                else {
-                    printf("DATA RECEIVED = %s\n", buf);
-                }
-                // checking clinet entry
-                if (strcmp(buf,"exit") == 0) {
-                    write(p[1], buf, MSGSIZE);
-                    break;
-                } else if (strcmp(buf, "max") == 0) {
-                    write(p[1], buf, MSGSIZE);
-                } else if (strcmp(buf, "min") == 0) {
-                    write(p[1], buf, MSGSIZE);
-                } else if (strcmp(buf, "dec") == 0) {
-                    write(p[1], buf, MSGSIZE);
-                } else if (strcmp(buf, "inc") == 0) {
-                    write(p[1], buf, MSGSIZE);
-                }
+            printf("socket listening...\n");
+        
+            /*********************************/
+            /* Accept an incoming connection */
+            /*********************************/
+            client_sock = accept(server_sock, (struct sockaddr *) &client_sockaddr, &len);
+            if (client_sock == -1){
+                printf("ACCEPT ERROR: %s\n", strerror(errno));
+                close(server_sock);
+                close(client_sock);
+                exit(1);
             }
             
-            /******************************/
-            /* Close the sockets and exit */
-            /******************************/
-            close(server_sock);
-            close(client_sock);
-            return 0;
+            /****************************************/
+            /* Get the name of the connected socket */
+            /****************************************/
+            len = sizeof(client_sockaddr);
+            rc = getpeername(client_sock, (struct sockaddr *) &client_sockaddr, &len);
+            if (rc == -1){
+                printf("GETPEERNAME ERROR: %s\n", strerror(errno));
+                close(server_sock);
+                close(client_sock);
+                exit(1);
+            }
+            else {
+                printf("Client socket filepath: %s\n", client_sockaddr.sun_path);
+            }
+        
+            /************************************/
+            /* Read and print the data          */
+            /* incoming on the connected socket */
+            /************************************/
+            printf("waiting to read...\n");
+            bytes_rec = recv(client_sock, buf, sizeof(buf), 0);
+            if (bytes_rec == -1){
+                printf("RECV ERROR: %s\n", strerror(errno));
+                close(server_sock);
+                close(client_sock);
+                exit(1);
+            }
+            else {
+                printf("DATA RECEIVED = %s\n", buf);
+            }
+            // checking clinet entry
+            if (strcmp(buf,"exit") == 0) {
+                write(p[1], buf, MSGSIZE);
+                break;
+            } else if (strcmp(buf, "max") == 0) {
+                write(p[1], buf, MSGSIZE);
+            } else if (strcmp(buf, "min") == 0) {
+                write(p[1], buf, MSGSIZE);
+            } else if (strcmp(buf, "dec") == 0) {
+                write(p[1], buf, MSGSIZE);
+            } else if (strcmp(buf, "inc") == 0) {
+                write(p[1], buf, MSGSIZE);
+            }
         }
+        
+        /******************************/
+        /* Close the sockets and exit */
+        /******************************/
+        close(server_sock);
+        close(client_sock);
+        return 0;
+
     } else {
+        // parent
+
         // getting into needed dir
         if (chdir("/sys/class/backlight/") < 0) {
             perror("chdir()");
